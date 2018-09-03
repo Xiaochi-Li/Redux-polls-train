d16c9f4d9dc5d53b3ceeaed0f3317c4a5c9e42dc
sean.li <sean.li@ladbrokes.com.au>
2018-09-03 14:28:34 +1000

    poll list

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {getPercentage} from '../utils/helpers'
+
+const getVoteKeys = () => ['aVotes', 'bVotes', 'cVotes', 'dVotes']
+
 class Poll extends Component {
   render() {
+    const { poll, vote, authorAvatar } = this.props
+
+    const totalVotes = getVoteKeys()
+      .reduce((total, key) => total + poll[key].length, 0)
+
+    if (poll === null) {
+      return <p>This poll does not exist</p>
+    }
     return (
       <div className='poll-container'>
-        {JSON.stringify(this.props)}
+        <h1 className="question">{poll.question}</h1>
+        <div className='poll-author'>
+          By <img src={authorAvatar} alt="Author's avatar" />
+        </div>
+        <ul>
+          {['a', 'b', 'c', 'd'].map((key) => {
+            const count = poll[key + 'Votes'].length
+            return (
+              <li key={key} className={`option ${vote === key[0] ? 'chosen' : ''}`}>
+                {poll[key + 'Votes'] === null
+                  ? poll[key + 'Text']
+                  : <div className='result'>
+                    <span>{poll[key + 'Text']}</span>
+                    <span>{getPercentage(count,totalVotes)}% ({count})</span>
+                  </div>
+                }
+              </li>)
+          })}
+        </ul>
       </div>
     )
   }
 }
 function mapStateToProps({ authedUser, polls, users }, { match }) {
-  console.log(match)
   const { id } = match.params
   const poll = polls[id]
   if (!poll) {
@@ -18,7 +47,7 @@ function mapStateToProps({ authedUser, polls, users }, { match }) {
       poll: null
     }
   }
-  const vote = ['aVotes', 'bVotes', 'cVotes', 'dVotes'].reduce((vote, key) => {
+  const vote = getVoteKeys().reduce((vote, key) => {
     if (vote !== null) {
       return vote[0]
     }
